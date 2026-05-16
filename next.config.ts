68b5c66f59e6720a20e5662336d0dadfcec60cb0
Tadelachewu <tade2024bdu@gmail.com>
2026-05-16 16:05:28 +0300

    prisma 7

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: [],
+  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
